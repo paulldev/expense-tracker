0d0c2ab3fe5d55b71189d7fa2649b79daa2be24d
Paul Lucey <theplantbasedking@gmail.com>
2022-08-15 16:09:06 +0200

    Create AllExpenses.js

---
+++ screens/AllExpenses.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+
+function AllExpenses() {
+  return <ExpensesOutput expensesPeriod="Total" />;
+}
+
+export default AllExpenses;
