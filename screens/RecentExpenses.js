59a2ee2b230999f0a9d565e9f1f116c6ca5c5784
Paul Lucey <theplantbasedking@gmail.com>
2022-08-15 16:09:53 +0200

    Create RecentExpenses.js

---
+++ screens/RecentExpenses.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+
+function RecentExpenses() {
+  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
+}
+
+export default RecentExpenses;
