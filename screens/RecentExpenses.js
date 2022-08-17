621800d81b572165c461dc3c7b5f19ac2fbd3f47
Paul Lucey <theplantbasedking@gmail.com>
2022-08-17 17:47:26 +0200

    Add context

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
+import { getDateMinusDays } from "../util/Date";
 
 function RecentExpenses() {
-  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
+  const expensesCtx = useContext(ExpensesContext);
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date(); //gets todays date
+    const date7DaysAgo = getDateMinusDays(today, 7);
+    return expense.date >= date7DaysAgo && expense.date <= today;
+  });
+  return (
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Last 7 Days"
+      fallbackText="No expenses found for the last 7 days"
+    />
+  );
 }
 
 export default RecentExpenses;
