c702d32cbe16ce281384fcd2c98f14c72b90d804
Paul Lucey <theplantbasedking@gmail.com>
2022-08-20 14:47:17 +0200

    Add storeExpense

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -5,6 +5,7 @@ import Button from "../components/UI/Button";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import { ExpensesContext } from "../store/expenses-context";
+import { storeExpense } from "../util/http";
 
 function ManageExpense({ route, navigation }) {
   const expensesCtx = useContext(ExpensesContext);
@@ -34,6 +35,7 @@ function ManageExpense({ route, navigation }) {
     if (isEditing) {
       expensesCtx.updateExpense(editedExpenseId, expenseData);
     } else {
+      storeExpense(expenseData);
       expensesCtx.addExpense(expenseData);
     }
     navigation.goBack();
