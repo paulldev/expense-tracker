3e41b8e47a1b0bbe2c992672b0bbf5fff733bb6c
Paul Lucey <theplantbasedking@gmail.com>
2022-08-20 19:13:46 +0200

    Create fetchExpenses()

--- util/http.js
+++ util/http.js
@@ -1,8 +1,29 @@
 import axios from "axios";
 
-export function storeExpense(expenseData) {
-  axios.post(
-    "https://plantbasedking-e175d-default-rtdb.firebaseio.com/expenses.json",
+const BACKEND_URL = "https://plantbasedking-e175d-default-rtdb.firebaseio.com";
+
+export async function storeExpense(expenseData) {
+  const response = await axios.post(
+    BACKEND_URL + "/expenses.json",
     expenseData
   );
+  const id = response.data.name; //firebase's auto-generated id
+  return id;
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + "/expenses.json");
+
+  const expenses = [];
+  console.log(response.data);
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+  return expenses;
 }
