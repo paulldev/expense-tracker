d8ae3933629578b27321e1c53a9a120d3b2319e2
Paul Lucey <theplantbasedking@gmail.com>
2022-08-20 22:11:22 +0200

    Add isFetching state

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,21 +1,29 @@
 import React, { useContext, useEffect, useState } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
 import { fetchExpenses } from "../util/http";
 
 function RecentExpenses() {
+  const [isFetching, setIsFetching] = useState(true);
   const expensesCtx = useContext(ExpensesContext);
   //const [fetchedExpenses, setFetchedExpenses] = useState([]);
 
   useEffect(() => {
     async function getExpenses() {
+      setIsFetching(true);
       const expenses = await fetchExpenses();
+      setIsFetching(false);
       //      setFetchedExpenses(expenses);
       expensesCtx.setExpenses(expenses);
     }
     getExpenses();
   }, []);
+
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
 
   //  const recentExpenses = expensesCtx.expenses.filter((expense) => {
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
