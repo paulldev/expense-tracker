f432589b0dd4fb14dd6d9dae6a52eb8722055c56
Paul Lucey <theplantbasedking@gmail.com>
2022-08-20 19:11:54 +0200

    Add useEffect

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,13 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../util/http";
 
 function RecentExpenses() {
   const expensesCtx = useContext(ExpensesContext);
+  //const [fetchedExpenses, setFetchedExpenses] = useState([]);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpenses();
+      //      setFetchedExpenses(expenses);
+      expensesCtx.setExpenses(expenses);
+    }
+    getExpenses();
+  }, []);
+
+  //  const recentExpenses = expensesCtx.expenses.filter((expense) => {
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date(); //gets todays date
     const date7DaysAgo = getDateMinusDays(today, 7);
+
     return expense.date >= date7DaysAgo && expense.date <= today;
   });
   return (
